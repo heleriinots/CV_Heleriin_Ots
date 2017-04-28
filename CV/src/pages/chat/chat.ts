1647452c0231faeb76914464ee178a66e175ec97
Heleriin Ots <heleot@ttu.ee>
2017-04-28 23:31:56 +0300

    Made the design look a bit more polished

--- CV/src/pages/chat/chat.ts
+++ CV/src/pages/chat/chat.ts
@@ -15,43 +15,136 @@ export class ChatPage {
 
   chat_input: string;
   messages = [];
+  count = 0;
 
   constructor() {
-    /*
     if (this.messages.length === 0) {
-      this.messages = JSON.parse(localStorage.getItem("messages"));
+      //this.messages = JSON.parse(localStorage.getItem("messages"));
+      if (this.messages.length == 0) {
+        this.startChat();
+      }
     }
-    */
   }
 
-
-  parseTime(time) {
-    return  moment(time).fromNow();
+  startChat() {
+    let time = moment();
+    this.messages.unshift({author: 'Chatbot', message: "Hello, what's your name?", class: 'chatbot', time: time});
   }
-
 
   eventHandler(keyCode, message) {
-    if(keyCode === 13) {
+    if (keyCode === 13) {
       this.send(message);
     }
   }
 
+  sendMessageOne(message) {
+    this.messages.unshift({
+      author: 'Guest',
+      message: message,
+      class: 'guest',
+      time: moment()
+    });
+
+    this.messages.unshift({
+      author: 'Chatbot',
+      message: "Hi, " + message + "! My name is Chatbot!",
+      class: 'chatbot',
+      time: moment()
+    });
+
+    this.messages.unshift({
+      author: 'Chatbot',
+      message: "I really like programming, how about you?",
+      class: 'chatbot',
+      time: moment()
+    });
+    this.count++;
+  }
+
+  sendMessageTwo(message) {
+    this.messages.unshift({
+      author: 'Guest',
+      message: message,
+      class: 'guest',
+      time: moment()
+    });
+
+    this.messages.unshift({
+      author: 'Chatbot',
+      message: "Oh, how interesting...",
+      class: 'chatbot',
+      time: moment()
+    });
+
+    this.messages.unshift({
+      author: 'Chatbot',
+      message: "What's the weather like?",
+      class: 'chatbot',
+      time: moment()
+    });
+
+    this.count++;
+  }
+
+  sendMessageThree(message) {
+    this.messages.unshift({
+      author: 'Guest',
+      message: message,
+      class: 'guest',
+      time: moment()
+    });
+
+    this.messages.unshift({
+      author: 'Chatbot',
+      message: "I don't get outdoors much, it's hard to tell that the weather is " + message,
+      class: 'chatbot',
+      time: moment()
+    });
+
+    this.messages.unshift({
+      author: 'Chatbot',
+      message: "I hope you like this app!",
+      class: 'chatbot',
+      time: moment()
+    });
+
+    this.count++;
+  }
+
+
   send(message) {
     if (message != '' && message != null) {
-      var time = moment();
-      this.messages.unshift({author: 'Guest', message: message, class: 'guest', time: time});
-      this.generateChatbotMessage();
+
+      if (this.count === 0) {
+        this.sendMessageOne(message);
+      } else if (this.count === 1) {
+        this.sendMessageTwo(message);
+      } else if (this.count === 2) {
+        this.sendMessageThree(message);
+      } else {
+
+        this.messages.unshift({author: 'Guest', message: message, class: 'guest', time: moment()});
+        this.generateChatbotMessage();
+      }
     }
 
-    console.log(JSON.stringify(this.messages));
     localStorage.setItem("messages", JSON.stringify(this.messages));
     this.chat_input = '';
   }
 
+
+  formatTime(time) {
+    return moment(time).fromNow();
+  }
+
+
   generateChatbotMessage() {
-    var time = moment();
+    let time = moment();
     this.messages.unshift({author: 'Chatbot', message: this.generateRandomSentence(), class: 'chatbot', time: time});
   }
+
+
+  ///FIX THIS!!!!!!
 
 
   verbs = [
@@ -59,44 +152,41 @@ export class ChatPage {
     //["look at", "looks at", "looking at", "looked at", "looked at"],
     //["choose", "chooses", "choosing", "chose", "chosen"],
     ["like", "likes", "liking", "liked", "liked"],
-    ["love", "loves", "loving", "loved", "loved"]
+    ["program", "programs", "programming", "programmed", "programmed"],
+    ["look", "looks", "looking", "looked", "looked"]
   ];
 
   tenses = [
-    {name:"Present", singular:1, plural:0, format:"%subject %verb %complement"},
-    {name:"Past", singular:3, plural:3, format:"%subject %verb %complement"}
-    //{name:"Present Continues", singular:2, plural:2, format:"%subject %be %verb %complement"}
-    ];
+    {name: "Present", singular: 1, plural: 0, format: " %subject %verb %complement"}
+  ];
 
   subjects = [
-    {name:"I", be:"am", singular:0},
-    {name:"You", be:"are", singular:0}
-    //{name:"He", be:"is", singular:1}
-    ];
+    {name: "I", be: "am", singular: 0},
+    {name: "You", be: "are", singular: 0},
+    {name: "We", be: "are", singular: 0}
+  ];
 
   complementsForVerbs = [
-    //["cinema", "Egypt", "home", "concert"],
-    //["for a map", "them", "the stars", "the lake"],
-    //["a book for reading", "a dvd for tonight"],
-    ["php", "java", "this app", "css", "programming", "talking with you", "javascript", "python"],
-    ["php", "java", "this app", "css", "programming", "talking with you", "javascript", "python"]];
+    ["php", "java", "this app", "css", "node.js", "talking with you", "javascript", "python", "bash", "html"],
+    ["a website", "with this new computer"],
+    ["at this code", "at this cool design"]
+  ];
 
 
   random(words) {
-    return words[Math.floor(Math.random()*words.length)];
+    return words[Math.floor(Math.random() * words.length)];
   }
 
 
   generateRandomSentence() {
 
-    //Array.prototype.random = function(){return this[Math.floor(Math.random() * this.length)];};
 
-    var index = Math.floor(this.verbs.length * Math.random());
-    var tense = this.random(this.tenses);
-    var subject = this.random(this.subjects);
-    var verb = this.verbs[index];
-    var complement = this.complementsForVerbs[index];
-    var str = tense.format;
+    let index = Math.floor(this.verbs.length * Math.random());
+    let tense = this.random(this.tenses);
+    let subject = this.random(this.subjects);
+    let verb = this.verbs[index];
+    let complement = this.complementsForVerbs[index];
+    let str = tense.format;
     str = str.replace("%subject", subject.name).replace("%be", subject.be);
     str = str.replace("%verb", verb[subject.singular ? tense.singular : tense.plural]);
     str = str.replace("%complement", this.random(complement));
--- CV/src/pages/home/home.ts
+++ CV/src/pages/home/home.ts
@@ -1,33 +1,17 @@
 import {Component} from "@angular/core";
-import {NavController} from "ionic-angular";
-import {PeopleService} from "../../providers/people-service";
-
 
 @Component({
   selector: 'home-ionic',
-  templateUrl: 'home.html',
-  providers: [PeopleService]
+  templateUrl: 'home.html'
 })
 
 export class HomePage {
-
-  public people: any;
 
   slideData = [{title: "Slide 1", info: "Additional info 1"}, {title: "Slide 2", info: "Additional info 1"},
     {title: "Slide 3", info: "Additional info 3"}]
 
 
-  constructor(public navCtrl: NavController, public peopleService: PeopleService) {
-    this.loadPeople();
-  }
-
-
-  loadPeople() {
-    this.peopleService.load()
-      .then(data => {
-        this.people = data;
-        console.log(this.people);
-      });
+  constructor() {
   }
 
 }
