27d839624e0fb77e43ae5e11f78e6d04bbdb779e
Heleriin Ots <heleot@ttu.ee>
2017-04-29 23:13:18 +0300

    Updated cv data

--- API/database/createTestData.sql
+++ API/database/createTestData.sql
@@ -18,13 +18,13 @@ INSERT INTO QualificationsInfo (id, type, details)
 VALUES (5, 'Email', 'heleriinots@gmail.com');
 
 INSERT INTO QualificationsInfo (id, type, details)
-VALUES (6,'Phone Numer', '59006626');
+VALUES (6, 'Phone Numer', '59006626');
 
 INSERT INTO QualificationsInfo (id, type, details)
 VALUES (7, 'Address', 'Kärberi 33-45, Tallinn, 13919');
 
 INSERT INTO QualificationsInfo (id, type, details)
-VALUES (8,'Education', '2015- Tallinn University of Technology, Bachelor of Informatics
+VALUES (8, 'Education', '2015- Tallinn University of Technology, Bachelor of Informatics
 2004-2015 Tallinn French Lyceum, Gold medalist
 2003-2004 Tallinn Kuristiku Secondary School
 2013-2014 Gustav Adolf School of Music
@@ -47,7 +47,12 @@ INSERT INTO QualificationsInfo (id, type, details)
 VALUES (11, 'Driver\'s License', 'B category driver\'s license');
 
 INSERT INTO QualificationsInfo (id, type, details)
-VALUES (12, 'IT Skills', 'dunno');
+VALUES (12, 'IT Skills', 'I would consider Java to be the language that I have the most experience with, since I have
+ successfully passed two Java courses at TTU and I am currently working as an assistant teacher of the Principal
+ Course of Programming in Java. I have also practiced quite a bit of Python and worked as an assitant teacher for the
+  In-depth Basic Course of Programming, which is a Python course. I have grasped the basics of C++, JavaScript,
+  jQuery, PHP, html, CSS and Sass and I have some experience with Node.js, AngularJS and Angular2. With regard to databases I have previously studied and used SQL, mainly in combination with MySQL. Currently I am working on an ionic application in a team of developers for the European Universities Debating Championship 2017. This project has taught me a lot about which tools and practices are used to develop larger projects, as well as how teamwork and project management are usually organised in IT companies.');
 
 INSERT INTO QualificationsInfo (id, type, details)
-VALUES (13, 'Hobbies', 'dabating, guitar, singing, drawing, write a description');
+VALUES (13, 'Hobbies', 'During my spare time I enjoy drawing and playing musical instruments, mainly the guitar and
+occasionally the piano. I am also an active debater and a board member of the TTU Debate Society.');
